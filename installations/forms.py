6c1ce0c431ea6555364a45f1c6ee5f5de8bdc56d
Kandroodi <mojtaba.rostami@ut.ac.ir>
2020-04-16 19:40:28 +0200

    Nav bar and delete confirmation

--- installations/forms.py
+++ installations/forms.py
@@ -1,11 +1,11 @@
 from django.forms import ModelForm
-from .models import City
+from .models import City, Institution
 
 
 class CityForm(ModelForm):
     class Meta:
         model = City
-        fields = ['name', 'country', 'latitude', 'longitude']
+        fields = ['name', 'latitude', 'longitude']
         # its possible to use following line for all fields, also exclude
         # fields = '__all__'
         labels = {
@@ -14,6 +14,22 @@ class CityForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(CityForm, self).__init__(*args, **kwargs)
-        self.fields['country'].empty_label = "Select"
+        # self.fields['country'].empty_label = "Select"
         self.fields['latitude'].required = False
         self.fields['longitude'].required = False
+
+
+class InstitutionForm(ModelForm):
+    class Meta:
+        model = Institution
+        fields = '__all__'
+
+        labels = {
+            'name': 'Institution Name'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(InstitutionForm, self).__init__(*args, **kwargs)
+        # self.fields['country'].empty_label = "Select"
+        # self.fields['latitude'].required = False
+        # self.fields['longitude'].required = False
--- installations/urls.py
+++ installations/urls.py
@@ -3,8 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.Home, name='home'), # get and post req. for insert operation
     path('city/new/', views.CityCreate, name='city-insert'), # get and post req. for insert operation
     path('city/new/<int:id>/', views.CityCreate, name='city-update'), # get and post req. for update operation
     path('city/delete/<int:id>/', views.CityDelete, name='city-delete'),
-    path('city/list/', views.CityList, name='city-list')  # get request to retrieve and display all records
+    path('city/list/', views.CityList, name='city-list'),  # get request to retrieve and display all records
+    path('institution/new/', views.InstitutionCreate, name='institution-insert'),  # get and post req. for insert operation
+    path('institution/new/<int:id>/', views.InstitutionCreate, name='institution-update'),  # get and post req. for update operation
+    path('institution/delete/<int:id>/', views.InstitutionDelete, name='institution-delete'),
+    path('institution/list/', views.InstitutionList, name='institution-list')  # get request to retrieve and display all records
 ]
