2b4cf61a1a8ba3d39a77966fcdb670ee8b31d0b0
Kandroodi <mojtaba.rostami@ut.ac.ir>
2020-06-04 10:09:54 +0200

    Add admin tab

--- waterSystem/urls.py
+++ waterSystem/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
     url('', include(('installations.urls', 'installations'), namespace="installations")),
 ]
