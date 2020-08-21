7ce267306c9e04ce22dc93329931ed14a75eaecd
kandroodi <mojtaba.rostami@ut.ac.ir>
2020-08-21 19:09:46 +0200

    template & static directory correction

--- waterSystem/settings.py
+++ waterSystem/settings.py
@@ -14,9 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'installations/templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'installations/static')
+MEDIA_DIR = os.path.join(BASE_DIR, 'installations/media')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
