0949760bf6372f1281b09447eb06e552e617e8af
Kandroodi <m.rostamikandroodi@let.ru.nl>
2021-08-20 14:41:37 +0200

    template modifications for advanced search, issue #48

--- installations/filters.py
+++ installations/filters.py
@@ -3,18 +3,19 @@ from .models import *
 
 
 class InstallationFilter(django_filters.FilterSet):
+    un_name = django_filters.CharFilter(field_name='un_name', lookup_expr='icontains', label='Name')
+    watersystem__original_term = django_filters.CharFilter(lookup_expr='icontains', label='Water system')
+    purpose__name = django_filters.CharFilter(lookup_expr='icontains', label='Purpose')
+    city__name = django_filters.CharFilter(lookup_expr='icontains', label='City')
+    neighbourhood__neighbourhood_number = django_filters.CharFilter(lookup_expr='icontains',
+                                                                    label='Neighbourhood number')
+    institution_as_location__name = django_filters.CharFilter(lookup_expr='icontains', label='Institution as location')
+    secondary_literature__title = django_filters.CharFilter(lookup_expr='icontains', label='Secondary literature')
+    un_comment = django_filters.CharFilter(lookup_expr='icontains', label='Comment')
+
     class Meta:
         model = Installation
         # fields =['name', 'watersystem__original_term']
         fields = {
-            'name': ['icontains'],
-            'watersystem__original_term': ['icontains'],
-            'purpose__name': ['icontains'],
-            'city__name': ['icontains'],
-            'neighbourhood__neighbourhood_number': ['icontains'],
-            'latitude': ['icontains'],
-            'longitude': ['icontains'],
-            'institution_as_location__name': ['icontains'],
-            'secondary_literature__title': ['icontains'],
-            'comment': ['icontains'],
+
         }
