24da484c6b9ba70a08414d11a58ef6d096600d7d
Kandroodi <m.rostamikandroodi@let.ru.nl>
2021-12-13 13:54:55 +0100

    media folder bug fixed

--- waterSystem/urls.py
+++ waterSystem/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path("select2/", include("django_select2.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
